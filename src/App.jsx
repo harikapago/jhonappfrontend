aba8b628aea06cc63006aedd6650dd7532798680
Harika Seelaboina <harika.seelaboina@homznoffiz.com>
2023-10-14 15:23:07 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import Visit from './Components/Visit'
 
 const App = () => {
   return (
-    <div className='' style={{width:""}}>
+    <div className='' style={{width:"98.6vw"}}>
      <Navbar/>
      <Topse/>
      <About/>
